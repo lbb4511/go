f87089b45cd4d8623798c0dbc80e0f76aeef2997
lbb4511 <lbb4511@qq.com>
2019-07-16 15:41:39 +0800

    gitalk

--- docs/_docsify.js
+++ docs/_docsify.js
@@ -17,7 +17,7 @@ window.$docsify = {
 const gitalk = new Gitalk({
   clientID: '8194e2d7dba9b720d111',
   clientSecret: '801009aef54ad4261602b62d58ec1f4d6c571ae4',
-  repo: 'go-study',
+  repo: 'go',
   owner: 'lbb4511',
   admin: ['lbb4511'],
   // facebook-like distraction free mode
