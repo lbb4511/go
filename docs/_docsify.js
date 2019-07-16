1022a90d3500f5fba54f421c7a19c823f87884ff
lbb4511 <lbb4511@qq.com>
2019-07-16 13:19:23 +0800

    gitalk

--- docs/_docsify.js
+++ docs/_docsify.js
@@ -17,9 +17,9 @@ window.$docsify = {
 const gitalk = new Gitalk({
   clientID: '8194e2d7dba9b720d111',
   clientSecret: '801009aef54ad4261602b62d58ec1f4d6c571ae4',
-  repo: 'https://github.com/lbb4511/go-study',
-  owner: 'Github repo owner',
-  admin: ['Github repo collaborators, only these guys can initialize github issues'],
+  repo: 'go-study',
+  owner: 'lbb4511',
+  admin: ['lbb4511'],
   // facebook-like distraction free mode
   distractionFreeMode: true,
   perPage: 10
